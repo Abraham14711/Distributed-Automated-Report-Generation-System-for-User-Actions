06c070c82df684c7a15b5484554c7c4eca81b2d4
Abraham14711 <pet_zed@rambler.ru>
2025-05-11 12:35:27 -0700

    feat: hadoop client

--- jobs/hadoop_client.go
+++ jobs/hadoop_client.go
@@ -2,10 +2,31 @@ package jobs
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/colinmarc/hdfs"
 )
 
 func main() {
-	client, err = hdfs.New(os.Getenv("HADOOP_HOST"))
+	client, err := hdfs.New(os.Getenv("HADOOP_HOST"))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	hdfsDir := "/data/reports"
+
+	files, err := client.ReadDir("/opt/spark/output_data")
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".parquet" {
+			localFile := "/opt/spark/output_data/" + file.Name()
+			remoteFile := hdfsDir + "/" + file.Name()
+			err = client.CopyToRemote(localFile, remoteFile)
+			if err != nil {
+				panic(err)
+			} else {
+				os.Remove(localFile)
+			}
+		}
+	}
 }
